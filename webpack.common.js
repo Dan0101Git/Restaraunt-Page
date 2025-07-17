9f17c278f796f2bd66ef8ffa6681c10477664855
Dan01011997 <f2016915@pilani.bits-pilani.ac.in>
2025-07-18 03:15:03 +0530

    split config.js into different modes

--- webpack.common.js
+++ webpack.common.js
@@ -3,16 +3,14 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+     publicPath: '/',
+         assetModuleFilename: 'images/[hash][ext][query]',
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
